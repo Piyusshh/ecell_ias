c1d7cb57987ac94d3ee11cca7fb85aab1fb41a0f
Piyush <Piyusshh@users.noreply.github.com>
2020-09-14 23:02:06 +0530

    password

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-$db_user = "root";
-$db_pass = "";
+$db_user = "ias20";
+$db_pass = "ecell123";
 $db_name = "ias_db";
 
 $db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
